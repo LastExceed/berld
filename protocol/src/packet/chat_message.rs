b222d3bc907648a4b59f9cd21dfcd39e43d58cdf
LastExceed <chris20194@googlemail.com>
2022-12-08 17:07:51 +0100

    minor `ChatMessage` de/serialization optimizations

--- protocol/src/packet/chat_message.rs
+++ protocol/src/packet/chat_message.rs
@@ -36,14 +36,14 @@ impl CwSerializable for ChatMessageFromServer {
 
 fn read_text(reader: &mut impl Read) -> Result<String, Error> {
 	let character_count = reader.read_struct::<i32>()? as usize;
+	const U16_SIZE: usize = size_of::<u16>();
 
-	let mut u8s = vec![0u8; character_count * 2];
+	let mut u8s = vec![0u8; character_count * U16_SIZE];
+
 	reader.read_exact(&mut u8s)?;
-
-	let window_size = size_of::<u16>();
 	let u16s = u8s
-		.windows(window_size)
-		.step_by(window_size)
+		.windows(U16_SIZE)
+		.step_by(U16_SIZE)
 		.map(|window| {
 			u16::from_le_bytes(
 				window
@@ -59,9 +59,9 @@ fn read_text(reader: &mut impl Read) -> Result<String, Error> {
 fn write_text(writer: &mut impl Write, string: &str) -> Result<(), Error> {
 	let bytes = string
 		.encode_utf16()
-		.flat_map(|u16character| u16character.to_le_bytes())
+		.flat_map(u16::to_le_bytes)
 		.collect::<Vec<u8>>();
-	let character_count = (bytes.len() / 2) as i32; //cant use use the utf16 iterator as counting it's elements would consume it prematurely
+	let character_count = (bytes.len() / 2) as i32; //cant use the utf16 iterator as counting it's elements would consume it prematurely
 	writer.write_struct(&character_count)?;
 	writer.write_all(&bytes)
 }
