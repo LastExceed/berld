2ff1fd195b5f531294bb756282673a9f000242e0
LastExceed <chris20194@googlemail.com>
2023-01-22 16:26:44 +0100

    apply damage balancing to posion

--- server/src/handle_packet/status_effect.rs
+++ server/src/handle_packet/status_effect.rs
@@ -1,3 +1,4 @@
+use std::sync::Arc;
 use std::time::Duration;
 
 use async_trait::async_trait;
@@ -5,7 +6,6 @@ use tokio::time::sleep;
 
 use protocol::nalgebra::Vector3;
 use protocol::packet::{Hit, StatusEffect, WorldUpdate};
-use protocol::packet::common::CreatureId;
 use protocol::packet::hit::HitType;
 use protocol::packet::status_effect::StatusEffectType;
 use protocol::packet::world_update::sound_effect::Sound;
@@ -25,13 +25,9 @@ impl HandlePacket<StatusEffect> for Server {
 				let players_guard = self.players.read().await; //todo: do i really have to do this?
 
 				let Some(target) = players_guard.iter().find(|player| { player.id == packet.target }) else {
-					return; //todo: invalid input? //can happen when the target disconnected in this moment
+					return; //can happen when the target disconnected in this moment
 				};
-				let target_owned = target.to_owned();
-				let packet_clone = packet.clone();
-				tokio::spawn(async move {
-					apply_poison(&target_owned, &packet_clone).await;
-				});
+				apply_poison(source, target.to_owned(), &packet).await;
 			}
 			StatusEffectType::WarFrenzy => {
 				balancing::buff_warfrenzy(&packet, self).await;
@@ -44,45 +40,52 @@ impl HandlePacket<StatusEffect> for Server {
 	}
 }
 
-async fn apply_poison(target: &Player, status_effect: &StatusEffect) {
+async fn apply_poison(source: &Player, target: Arc<Player>, status_effect: &StatusEffect) {
+	let source_creature_guard = source.creature.read().await;
+	let target_creature_guard = target.creature.read().await;
+
+	let mut hit = Hit {
+		attacker: source.id,//todo: check if this matters
+		target: status_effect.target,
+		damage: status_effect.modifier,
+		critical: false,
+		stuntime: 0,
+		position: target_creature_guard.position,
+		direction: Vector3::zeros(),
+		is_yellow: false,
+		type_: HitType::Normal,
+		flash: true,
+	};
+
+	balancing::adjust_hit(&mut hit, &source_creature_guard, &target_creature_guard);
+	drop(source_creature_guard);
+	drop(target_creature_guard);
+
+	let sound_effect = SoundEffect {
+		position: sound_position_of(hit.position),
+		sound: Sound::SlimeGroan,
+		pitch: 1.0,
+		volume: 1.0
+	};
+
+	let world_update = WorldUpdate {
+		hits: vec![hit],
+		sound_effects: vec![sound_effect],
+		..Default::default()
+	};
+
 	let tick_count = status_effect.duration / 500;
 
-	for i in 0..=tick_count {
-		if i != 0 {
-			sleep(Duration::from_millis(500)).await;
+	tokio::spawn(async move {
+		for i in 0..=tick_count {
+			if i != 0 {
+				sleep(Duration::from_millis(500)).await;
+			}
+
+			if let Err(_) = target.send(&world_update).await {
+				//disconnects are handled in the reading thread
+				break;
+			};
 		}
-
-		let target_position = target.creature.read().await.position;
-
-		let hit = Hit {
-			attacker: CreatureId(0),//todo: check if this matters
-			target: status_effect.target,
-			damage: status_effect.modifier,
-			critical: false,
-			stuntime: 0,
-			position: target_position,
-			direction: Vector3::zeros(),
-			is_yellow: false,
-			type_: HitType::Normal,
-			flash: true,
-		};
-
-		let sound_effect = SoundEffect {
-			position: sound_position_of(target_position),
-			sound: Sound::Absorb,
-			pitch: 1.0,
-			volume: 1.0
-		};
-
-		let world_update = WorldUpdate {
-			hits: vec![hit],
-			sound_effects: vec![sound_effect],
-			..Default::default()
-		};
-
-		if let Err(_) = target.send(&world_update).await {
-			//disconnects are handled in the reading thread
-			break;
-		};
-	}
+	});
 }
