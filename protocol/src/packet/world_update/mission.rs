1745f740eddd46c78893451dc4415c2bde6eb35d
LastExceed <chris20194@googlemail.com>
2023-02-08 15:52:27 +0100

    add mission objectives

--- protocol/src/packet/world_update/mission.rs
+++ protocol/src/packet/world_update/mission.rs
@@ -1,3 +1,22 @@
+#[repr(u32)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub enum Objective {
+	RemoveMission,//TODO: surrogate value
+	DefeatTheRaceInLocation,
+	BesiegeNameDenIn,
+	BesiegeDenInLocation,
+	BesiegeNameDenIm,
+	DefeatTheRulerInLocation,
+	Blank,
+	BesiegeDieRacesInDenLändernVon,
+	BesiegeDieRacesImLocation,
+	BesiegeDieRacesInLocation,
+	SpüreDieRacesInHöhlenAufUndBekämpfeSie,
+	SucheDieFlüsseImGanzenLandAbUndBekämpfeDieRaces,
+	BesiegeDieBandeImLocation,
+	BesiegeNameDenAnführerDerBanditenImLocation
+}
+
 #[repr(u8)]
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum MissionState {
