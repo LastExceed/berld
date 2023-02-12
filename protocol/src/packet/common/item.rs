66fbbbfdd49a4fa8ac2ac0364174e1a5678108dd
LastExceed <chris20194@googlemail.com>
2023-02-12 13:50:09 +0100

    remove special parsing logic for formulas and leftovers

--- protocol/src/packet/common/item.rs
+++ protocol/src/packet/common/item.rs
@@ -1,9 +1,6 @@
-use std::mem::{size_of, transmute};
-
-use async_trait::async_trait;
 use nalgebra::Point3;
 use strum_macros::{EnumCount, EnumIter};
-use tokio::io::{self, AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
+use tokio::io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
 
 use kind::*;
 
@@ -117,37 +114,6 @@ pub struct Spirit {
 	//pad2 //todo: struct align suggests that this could be a property, maybe seed/rarity/flags of the spirit?
 }
 
-#[async_trait]
-impl CwSerializable for Item {
-	async fn read_from<Readable: AsyncRead + Unpin + Send>(readable: &mut Readable) -> io::Result<Self>
-		where [(); size_of::<Self>()]:
-	{
-		let mut buffer = [0u8; size_of::<Self>()];
-		readable.read_exact(&mut buffer).await?;
-
-		//for formulas and leftovers, the resulting item combines the major portion of [recipe] with the minor portion of [kind]
-		//this makes type safe item kind handling impossible, as the minor portion of formulas and leftovers can be that of any item::Kind
-		//to sidestep this problem, we copy over the minor portion from [kind] to [recipe]
-		buffer[9] = buffer[1]; //todo: verify that the overwritten byte was 0
-		//this unfortunately overwrites the minor portion of [recipe], which is actually persistent memory ingame
-		//but since there is no known usecase it might just be the result of copy optimizations
-		//if it ever turns out to be something after all we can still move it into an ephemeral padding
-
-		Ok(unsafe { transmute(buffer) })
-	}
-
-	async fn write_to<Writable: AsyncWrite + Unpin + Send>(&self, writable: &mut Writable) -> io::Result<()> {
-		let mut buffer = unsafe { transmute::<_, [u8; size_of::<Self>()]>(self.clone()) };
-
-		//see above
-		if [2, 14].contains(&buffer[0]) { //todo: extract numbers from enum
-			buffer[1] = buffer[9];
-		}
-		buffer[9] = 0;
-
-		writable.write_all(&buffer).await
-	}
-}
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy, EnumIter, EnumCount)]
 pub enum Stat {
