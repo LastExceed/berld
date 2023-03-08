5db0fe770320fb617ab2c3c38f0ae1f77ce763f1
LastExceed <chris20194@googlemail.com>
2023-03-08 20:24:09 +0100

    add parameter capability to commands

--- server/src/addon/commands.rs
+++ server/src/addon/commands.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 use std::future::Future;
 use std::pin::Pin;
+use std::str::SplitWhitespace;
 
 use crate::server::player::Player;
 use crate::server::Server;
@@ -27,16 +28,23 @@ impl CommandManager {
 			return false;
 		}
 
-		let result = text[1..]
-			.split_whitespace()
+//		let mut fragments: Vec<_> = text[1..].split_whitespace()
+//			.map(|fragment| fragment.to_string()).collect(); //todo: figure out how to pass fragments by refer
+		let mut fragments = text[1..].split_whitespace();
+
+		let result = fragments
 			.next()
 			.ok_or("no command specified")
-			.and_then(|frag| self.commands.get(frag).ok_or("unknown command"));
+			.and_then(|frag| {
+				self.commands
+					.get(frag)
+					.ok_or("unknown command")
+			});
 //			.and_then(|command| command.get_execution_future(server, caller).await)
 //			.map_err(async move |error| caller.notify(error).await)
 
 		let result = match result {//unfortunately mapping functions dont support async/await, so we need to fallback to `match`
-			Ok(command) => command.get_execution_future(server, caller).await,
+			Ok(command) => command.get_execution_future(server, caller, &mut fragments).await,
 			Err(e) => Err(e)
 		};
 
@@ -51,14 +59,14 @@ impl CommandManager {
 pub trait Command {
 	const ADMIN_ONLY: bool;
 
-	fn execute<'fut>(&'fut self, server: &'fut Server, caller: &'fut Player) -> impl Future<Output=CommandResult> + Send + 'fut;//if you see an error here, ignore it -> https://github.com/intellij-rust/intellij-rust/issues/10216
+	fn execute<'fut>(&'fut self, server: &'fut Server, caller: &'fut Player, params: &'fut mut SplitWhitespace<'fut>) -> impl Future<Output=CommandResult> + Send + 'fut;//if you see an error here, ignore it -> https://github.com/intellij-rust/intellij-rust/issues/10216
 }
 
 //neither associated constants nor async functions are object safe, so we need a proxy for both
 trait ObjectSafeCommand: Send + Sync {//todo: Sync bound is only because of discord spaghetti {
 	fn get_admin_only(&self) -> bool;
 
-	fn get_execution_future<'fut>(&'fut self, server: &'fut Server, caller: &'fut Player) -> CommandFuture<'fut>;
+	fn get_execution_future<'fut>(&'fut self, server: &'fut Server, caller: &'fut Player, params: &'fut mut SplitWhitespace<'fut>) -> CommandFuture<'fut>;
 }
 
 impl<T: Command + Send + Sync> ObjectSafeCommand for T {
@@ -66,8 +74,8 @@ impl<T: Command + Send + Sync> ObjectSafeCommand for T {
 		T::ADMIN_ONLY
 	}
 
-	fn get_execution_future<'fut>(&'fut self, server: &'fut Server, caller: &'fut Player) -> CommandFuture<'fut> {
-		Box::pin(self.execute(server, caller))
+	fn get_execution_future<'fut>(&'fut self, server: &'fut Server, caller: &'fut Player, params: &'fut mut SplitWhitespace<'fut>) -> CommandFuture<'fut> {
+		Box::pin(self.execute(server, caller, params))
 	}
 }
 
@@ -77,8 +85,14 @@ struct Xp;
 impl Command for Xp {
 	const ADMIN_ONLY: bool = false;
 
-	async fn execute(&self, _server: &Server, caller: &Player) -> CommandResult {
-		give_xp(caller, 42).await;
+	async fn execute(&self, _server: &Server, caller: &Player, params: &mut SplitWhitespace<'_>) -> CommandResult {
+		let amount: i32 = params
+			.next()
+			.ok_or("no amount specified")?
+			.parse()
+			.map_err(|_| "invalid amount specified")?;
+
+		give_xp(caller, amount).await;
 
 		Ok(())
 	}
