a3af233164d24e3b0cb29094b32fa4e4f489be55
LastExceed <chris20194@googlemail.com>
2023-12-02 19:28:34 +0100

    add some testing commands

--- server/src/addon/command_manager/commands/test.rs
+++ server/src/addon/command_manager/commands/test.rs
@@ -1,42 +1,144 @@
+use std::ops::{Div, Mul};
 use std::str::SplitWhitespace;
 
-use strum::IntoEnumIterator;
-
-use protocol::packet::world_update::{Sound, sound};
+use protocol::nalgebra::{Point3, Vector3};
+use protocol::packet::common::Hitbox;
+use protocol::packet::world_update::{Block, WorldObject};
+use protocol::packet::world_update::block::Kind::Solid;
+use protocol::packet::world_update::world_object::Kind::Crate;
 use protocol::packet::WorldUpdate;
-use protocol::utils::sound_position_of;
+use protocol::utils::constants::{SIZE_BLOCK, SIZE_ZONE};
 
 use crate::addon::command_manager::{Command, CommandResult};
 use crate::addon::command_manager::commands::Test;
 use crate::addon::command_manager::utils::INGAME_ONLY;
+use crate::server::creature::Creature;
 use crate::server::player::Player;
 use crate::server::Server;
 
 impl Command for Test {
 	const LITERAL: &'static str = "t";
-	const ADMIN_ONLY: bool = true;
+	const ADMIN_ONLY: bool = false;
 
 	async fn execute<'fut>(&'fut self, _server: &'fut Server, caller: Option<&'fut Player>, params: &'fut mut SplitWhitespace<'fut>) -> CommandResult {
 		let caller = caller.ok_or(INGAME_ONLY)?;
+		let character = caller.character.read().await;
 
-		let sound = Sound {
-			position: sound_position_of(caller.character.read().await.position),
-			kind: sound::Kind::iter()
-				.nth(
-					params
-						.next()
-						.ok_or("no param")?
-						.parse()
-						.map_err(|_| "parse failed")?
-				)
-				.ok_or("out of bounds")?,
-			pitch: params
-				.next()
-				.map_or(1.0, |input| input.parse().unwrap()),
-			volume: 1.0,
-		};
-		caller.send_ignoring(&WorldUpdate::from(sound)).await;
+		match params.next() {
+			Some("check") => checkerboard(caller, &character).await,
+			Some("obj") => world_object(caller, &character).await,
+			Some("block") => place_block(caller, &character).await,
+			Some(_) => { return Err("unknown sub-command") }
+			None => { return Err("too few arguments") },
+		}
 
 		Ok(None)
 	}
+}
+
+async fn place_block(caller: &Player, character: &Creature) {
+	let block = Block {
+		position: character.position.map(|scalar| (scalar / SIZE_BLOCK) as _),
+		color: [0,0,0].into(),
+		kind: Solid,
+		padding: 0,
+	};
+
+	caller.send_ignoring(&WorldUpdate::from(block)).await;
+}
+
+async fn world_object(caller: &Player, character: &Creature) {
+	let object = WorldObject {
+		zone: character.position.xy().map(|scalar| (scalar / SIZE_ZONE) as _),
+		id: 0,
+		position: character.position,
+		orientation: 0,
+		size: Hitbox {
+			width: 5.0,
+			height: 5.0,
+			depth: 5.0
+		},
+		is_closed: false,
+		transform_time: 0,
+		unknown_b: 0,
+		kind: Crate,
+
+		unknown_a: 0,
+		interactor: caller.id.0,
+	};
+
+	caller.send_ignoring(&WorldUpdate::from(object)).await;
+}
+
+async fn checkerboard(caller: &Player, character: &Creature) {
+	let start = Point3::new(
+		character.position.x
+			.div(SIZE_ZONE)
+			.mul(SIZE_ZONE)
+			.div(SIZE_BLOCK) as i32,
+		character.position.y
+			.div(SIZE_ZONE)
+			.mul(SIZE_ZONE)
+			.div(SIZE_BLOCK) as i32,
+		character.position.z.div(SIZE_BLOCK) as i32,
+	);
+
+	let mut blocks = Vec::with_capacity(100);
+
+	for dx in 0..300 {
+		for dy in 0..300 {
+			let block_alt = (dx + dy) % 2 == 1;
+			let mapblock_alt = ((dx / 8) + (dy / 8)) % 2 == 1;
+			let chunk_alt = ((dx / 32) + (dy / 32)) % 2 == 1;
+			let zone_alt = ((dx / 256) + (dy / 256)) % 2 == 1;
+
+			#[expect(clippy::collapsible_else_if, reason = "TODO")]
+			let color =
+				if zone_alt {
+					if chunk_alt {
+						if mapblock_alt {
+							if block_alt { [0, 192, 192] } else { [0, 255, 255] }
+						} else {
+							if block_alt { [0, 192, 0] } else { [0, 255, 0] }
+						}
+					} else {
+						if mapblock_alt {
+							if block_alt { [0, 96, 192] } else { [0, 128, 255] }
+						} else {
+							if block_alt { [0, 0, 128] } else { [0, 0, 255] }
+						}
+					}
+				} else {
+					if chunk_alt {
+						if mapblock_alt {
+							if block_alt { [192, 0, 96] } else { [255, 0, 128] }
+						} else {
+							if block_alt { [96, 0, 96] } else { [255, 0, 255] }
+						}
+					} else {
+						if mapblock_alt {
+							if block_alt { [192, 96, 0] } else { [255, 128, 0] }
+						} else {
+							if block_alt { [128, 0, 0] } else { [255, 0, 0] }
+						}
+					}
+				}.into();
+
+			let block = Block {
+				position: start + Vector3::new(dx, dy, 0),
+				kind: Solid,
+				color,
+				padding: 0
+			};
+
+			blocks.push(block);
+		}
+	}
+
+	let wu = WorldUpdate {
+		blocks,
+		..Default::default()
+	};
+
+	caller.send_ignoring(&wu).await;
 }
