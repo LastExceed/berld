df1012c2ed866693e8b8b529bdc3289016828626
LastExceed <chris20194@googlemail.com>
2023-12-08 21:34:43 +0100

    some minor cleanup

--- server/src/addon/command_manager/commands/act.rs
+++ server/src/addon/command_manager/commands/act.rs
@@ -1,5 +1,6 @@
 use std::str::SplitWhitespace;
 use std::time::Duration;
+use tokio::time::sleep;
 use protocol::nalgebra::Point2;
 use protocol::packet::WorldUpdate;
 use protocol::packet::common::Hitbox;
@@ -23,19 +24,19 @@ impl Command for Act {
 
 		let action: &str = params
 			.next()
-			.ok_or("no amount specified")?;
+			.ok_or("no action specified")?;
 
 		let kind = match action {
 			"sit" => Kind::Bench,
 			"sleep" => Kind::SleepingMat,
-			_ => {return Err("Unknown action. try: [sit, sleep]")}
+			_ => { return Err("Unknown action. try: [sit, sleep]") }
 		};
 		let zone = Point2::new(
 			(character_guard.position[0] / SIZE_ZONE) as i32,
 			(character_guard.position[1] / SIZE_ZONE) as i32
 		);
 
-		let world_object = WorldObject{
+		let world_object = WorldObject {
 			zone,
 			id: 0,
 			unknown_a: 0,
@@ -48,15 +49,16 @@ impl Command for Act {
 			unknown_b: 0,
 			interactor: caller.id.0,
 		};
+		drop(character_guard);
 
-		caller.send_ignoring(&WorldUpdate{
+		caller.send_ignoring(&WorldUpdate {
 			world_objects: vec![world_object.clone()],
 			..Default::default()
 		}).await;
 
-		tokio::time::sleep(Duration::from_millis(10)).await; // We need this or the packet is not affecting the player.
+		sleep(Duration::from_millis(10)).await; // We need this or the packet is not affecting the player.
 
-		caller.send_ignoring(&WorldUpdate{
+		caller.send_ignoring(&WorldUpdate {
 			world_objects: vec![WorldObject {
 				interactor: 0,
 				..world_object
