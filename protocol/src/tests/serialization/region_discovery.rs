6bb1eecccfd8e393fbedf0f380013d0c7c574fec
LastExceed <chris20194@googlemail.com>
2023-12-18 13:03:17 +0100

    fix some refactoring oversights in unit tests

--- protocol/src/tests/serialization/region_discovery.rs
+++ protocol/src/tests/serialization/region_discovery.rs
@@ -1,6 +1,6 @@
-use crate::generate_serialization_tests;
+use crate::{generate_serialization_tests, packet::area_request::Region};
 
 generate_serialization_tests!(
-	RegionDiscovery([0x12345678, 0x11223344].into()),
+	AreaRequest::<Region>([0x12345678, 0x11223344].into()),
 	[0x78, 0x56, 0x34, 0x12, 0x44, 0x33, 0x22, 0x11]
 );
--- protocol/src/tests/serialization/zone_discovery.rs
+++ protocol/src/tests/serialization/zone_discovery.rs
@@ -1,6 +1,6 @@
-use crate::generate_serialization_tests;
+use crate::{generate_serialization_tests, packet::area_request::Zone};
 
 generate_serialization_tests!(
-	ZoneDiscovery([0x12345678, 0x11223344].into()),
+	AreaRequest::<Zone>([0x12345678, 0x11223344].into()),
 	[0x78, 0x56, 0x34, 0x12, 0x44, 0x33, 0x22, 0x11]
 );
